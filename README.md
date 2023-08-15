7c2d6b65c60d088e6e85783c061e6b0c31c7629c
cumauu73 <cumauu73@icloud.com>
2023-08-14 23:18:37 -0500

    html for video

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 created logo based on question inquery 
 trianle, circle and square
+for the video clink link to open page
+[live page](https:)
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,11 +1,25 @@
-import * as Shapes from "../lib/shapes"
+import { Shape, Circle, Square, Triangle } from "../lib/shapes.js"
 
+describe("Circle Class", () => {
+    it("should return a string containing code for an SVG circle", () => {
+        const circle = new Circle()
+        expect(circle.render()).toEqual(`<circle cx="150" cy="105" r="80" fill="${circle.color}" />`)
+    })
 
-describe("", () => {
-    it("", () => {
-        // Arrange
-        // Act
-        // Assert
+})
+
+describe("Square Class", () => {
+    it("should return a string containing code for an SVG circle", () => {
+        const square = new Square()
+        expect(square.render()).toEqual(`<rect x="85" y="40" width="130" height="130" fill="${square.color}" />`)
+    })
+
+})
+
+describe("Triangle Class", () => {
+    it("should return a string containing code for an SVG circle", () => {
+        const triangle = new Triangle()
+        expect(triangle.render()).toEqual(`<polygon points="150, 10 280, 140 20, 140" fill="${triangle.color}" />`)
     })
 
 })
